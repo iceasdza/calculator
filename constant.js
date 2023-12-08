ff616965686591fbfd8e9b5926470dc19d4e8e31
iceasdza <roiceasd@gmail.com>
2023-12-08 21:40:31 +0700

    [ADD] init node

---
+++ constant.js
@@ -0,0 +1,16 @@
+export const MENU = [
+  { name: "Red Set", price: 50 },
+  { name: "Green Set", price: 40 },
+  { name: "Blue set", price: 30 },
+  { name: "Yellow Set", price: 50 },
+  { name: "Pink Set", price: 80 },
+  { name: "Purple Set", price: 90 },
+  { name: "Orange Set", price: 120 },
+];
+
+export const MEMBERSHIP_DISCOUNT_PERCENTAGE = 10;
+
+export const DOUBLE_ORDER_MENU_LIST = {
+  menu: ["Red Set", "Purple Set", "Orange Set"],
+  discountPercentage: 5,
+};
